0f6ca7501849f0752d99059c318d7fa1971e0ef8
Jorge domínguez <herrerajorge725@gmail.com>
2022-05-28 00:41:43 -0500

    Microservicio buscaame2

---
+++ buscame2/src/main/java/com/autobuses/buscame/BuscameEndPoint.java
@@ -0,0 +1,81 @@
+package com.autobuses.buscame;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import https.buscame_autobuses_com.buscame.CrearRequest;
+import https.buscame_autobuses_com.buscame.CrearResponse;
+import https.buscame_autobuses_com.buscame.ListarCarrosResponse;
+import https.buscame_autobuses_com.buscame.QuitarCarroRequest;
+import https.buscame_autobuses_com.buscame.QuitarCarroResponse;
+@Endpoint
+public class BuscameEndPoint
+{
+    @Autowired
+    private ClienteEndPoint cliente;
+    ArrayList<ListarCarrosResponse.Carro> listaCarros = new ArrayList<ListarCarrosResponse.Carro>();
+
+    @PayloadRoot(localPart = "CrearRequest", namespace = "https://buscame.autobuses.com/buscame")
+    @ResponsePayload
+    
+    public CrearResponse Crearautobus(@RequestPayload CrearRequest request)
+    {
+        
+
+        com.autobuses.wsdl.QuitarCarroResponse respuesta = cliente.eliminarCarro(request.getId());
+
+        CrearResponse response = new CrearResponse();
+        if(respuesta.isResponse())
+        {
+            ListarCarrosResponse.Carro carro = new ListarCarrosResponse.Carro();
+            carro.setId(request.getId());
+            carro.setConductor(request.getConductor());
+            carro.setEmpresa(request.getEmpresa());
+            carro.setRuta(request.getRuta());
+            carro.setPlaca(request.getPlaca());
+            listaCarros.add(carro);
+
+            response.setResponse(true);
+            return response;
+        }
+        else
+        {
+            response.setResponse(false);
+            return response;
+        }
+        
+    }
+
+    @PayloadRoot(localPart = "ListarCarrosRequest", namespace = "https://buscame.autobuses.com/buscame")
+    @ResponsePayload
+    public ListarCarrosResponse getAutobuses()
+    {
+        ListarCarrosResponse response = new ListarCarrosResponse();
+        for (ListarCarrosResponse.Carro carro : listaCarros) 
+        {
+            response.getCarro().add(carro);
+        }
+        return response;
+    }
+
+    @PayloadRoot(localPart = "QuitarCarroRequest", namespace = "https://buscame.autobuses.com/buscame")
+    @ResponsePayload
+    public QuitarCarroResponse deleteAutobus(@RequestPayload QuitarCarroRequest request)
+    {
+        QuitarCarroResponse response = new QuitarCarroResponse();
+        for(ListarCarrosResponse.Carro carro : listaCarros)
+        {
+            if(carro.getId() == request.getId())
+            {
+                listaCarros.remove(carro);
+            }
+        }
+        response.setResponse(true);
+        return response;
+    }
+}
